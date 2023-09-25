443ce0315d397aa2f7bc812792f986c5e23dd0d1
Vicegale <vicegale@vicegale.xyz>
2023-09-25 19:44:11 +0100

    sql!

--- src/app/task/new/actions.ts
+++ src/app/task/new/actions.ts
@@ -2,7 +2,11 @@
 
 import { TaskDefinition } from "@/types/task";
 import { z } from "zod";
+import { sql } from '@vercel/postgres';
 
 export async function createTaskDefinition(values: z.infer<typeof TaskDefinition>) {
     console.log(values);
+    const q = `INSERT INTO TaskDefinition (id, prompt, params) VALUES (1, '${values.prompt}', '${JSON.stringify(values.params)}'::json)`;
+    console.log(q);
+    await sql`${q}`
 }
