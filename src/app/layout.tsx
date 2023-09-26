38b993e85f68d514543d4fc31bb3289d70749992
Vicegale <vicegale@vicegale.xyz>
2023-09-26 15:41:20 +0100

    Clerk!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { ThemeProvider } from '@/components/theme-provider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider, UserButton } from '@clerk/nextjs'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,13 +18,15 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
-            <body className={inter.className}>
-                <ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange>
-                
-                    {children}
-                </ThemeProvider>
-            </body>
-        </html>
+        <ClerkProvider>
+            <html lang="en">
+                <body className={inter.className}>
+                    <ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange>
+                        <UserButton afterSignOutUrl="/"/>
+                        {children}
+                    </ThemeProvider>
+                </body>
+            </html>
+        </ClerkProvider>
     )
 }
--- src/app/task/new/actions.ts
+++ src/app/task/new/actions.ts
@@ -3,10 +3,14 @@
 import { TaskDefinition } from "@/types/task";
 import { z } from "zod";
 import { createClient } from "@libsql/client";
+import { currentUser } from "@clerk/nextjs/server";
 
 export async function createTaskDefinition(values: z.infer<typeof TaskDefinition>) {
+    const user = await currentUser();
+    const userId = user?.id;
+    console.log(userId);
     console.log(values);
-    const q = `INSERT INTO TaskDefinition (userId, prompt, parameters) VALUES ("google_id", '${values.prompt}', json('${JSON.stringify(values.params)}'))`;
+    const q = `INSERT INTO TaskDefinition (userId, prompt, parameters) VALUES ('${userId}', '${values.prompt}', json('${JSON.stringify(values.params)}'))`;
     console.log(q);
     const client = createClient({
         url: process.env.TURSO_URL!,
