71f26de4210d2e7f8c6cae9350d2ccc29e1b1bb8
Vicegale <vicegale@vicegale.xyz>
2023-09-27 03:13:44 +0100

    fix

--- src/app/task/[id]/page.tsx
+++ src/app/task/[id]/page.tsx
@@ -1,22 +1,22 @@
 import { getTaskDefinition } from './actions';
 
-export default async function Page({ params }:{ params: { id: number }}) {
+export default async function Page({ params }: { params: { id: number } }) {
     const definition = await getTaskDefinition(params.id);
 
     return (
         <div>
             <p>{definition?.prompt}</p>
             <div>
-            <p>Inputs</p>
-            <div>
-                {definition?.inputParams.map((i) => <p>{i.value}</p>)}
+                <p>Inputs</p>
+                <div>
+                    {definition?.inputParams.map((i, idx) => <p key={idx}>{i.value}</p>)}
+                </div>
             </div>
-            </div>
             <div>
-            <p>Output</p>
-            <div>
-                {definition?.outputParams.map((i) => <p>{i.value}</p>)}
-            </div>
+                <p>Output</p>
+                <div>
+                    {definition?.outputParams.map((i, idx) => <p key={idx}>{i.value}</p>)}
+                </div>
             </div>
         </div>
 
